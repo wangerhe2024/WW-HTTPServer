369b34102015747321ce630cbc9cf7faf80f61b5
WW <erhewang2024@163.com>
2025-08-09 21:04:11 +0800

    ...

--- include/utils/MysqlUtil.h
+++ include/utils/MysqlUtil.h
@@ -1,35 +1,30 @@
- #pragma once
- #include "db/DbConnectionPool.h"
- 
+#pragma once
+#include "db/DbConnectionPool.h"
+
 #include <string>
 
-namespace http
-{
+namespace ww_http {
 
-class MysqlUtil
-{
+class MysqlUtil {
 public:
-    static void init(const std::string& host, const std::string& user,
-                    const std::string& password, const std::string& database,
-                    size_t poolSize = 10)
-    {
-        http::db::DbConnectionPool::getInstance().init(
-            host, user, password, database, poolSize);
-    }
+  static void init(const std::string &host, const std::string &user,
+                   const std::string &password, const std::string &database,
+                   size_t poolSize = 10) {
+    ww_http::db::DbConnectionPool::getInstance().init(host, user, password,
+                                                      database, poolSize);
+  }
 
-    template<typename... Args>
-    sql::ResultSet* executeQuery(const std::string& sql, Args&&... args)
-    {
-        auto conn = http::db::DbConnectionPool::getInstance().getConnection();
-        return conn->executeQuery(sql, std::forward<Args>(args)...);
-    }
+  template <typename... Args>
+  sql::ResultSet *executeQuery(const std::string &sql, Args &&... args) {
+    auto conn = ww_http::db::DbConnectionPool::getInstance().getConnection();
+    return conn->executeQuery(sql, std::forward<Args>(args)...);
+  }
 
-    template<typename... Args>
-    int executeUpdate(const std::string& sql, Args&&... args)
-    {
-        auto conn = http::db::DbConnectionPool::getInstance().getConnection();
-        return conn->executeUpdate(sql, std::forward<Args>(args)...);
-    }
+  template <typename... Args>
+  int executeUpdate(const std::string &sql, Args &&... args) {
+    auto conn = ww_http::db::DbConnectionPool::getInstance().getConnection();
+    return conn->executeUpdate(sql, std::forward<Args>(args)...);
+  }
 };
 
-} // namespace http
+} // namespace ww_http
--- include/utils/db/DbConnectionPool.h
+++ include/utils/db/DbConnectionPool.h
@@ -1,61 +1,55 @@
 #pragma once
-#include <queue>
-#include <mutex>
+#include "DbConnection.h"
 #include <condition_variable>
 #include <memory>
+#include <mutex>
+#include <queue>
 #include <thread>
-#include "DbConnection.h"
 
-namespace http 
-{
-namespace db 
-{
+namespace ww_http {
+namespace db {
 
-class DbConnectionPool 
-{
+class DbConnectionPool {
 public:
-    // 单例模式
-    static DbConnectionPool& getInstance() 
-    {
-        static DbConnectionPool instance;
-        return instance;
-    }
+  // 单例模式
+  static DbConnectionPool &getInstance() {
+    static DbConnectionPool instance;
+    return instance;
+  }
 
-    // 初始化连接池
-    void init(const std::string& host,
-             const std::string& user,
-             const std::string& password,
-             const std::string& database,
-             size_t poolSize = 10);
+  // 初始化连接池
+  void init(const std::string &host, const std::string &user,
+            const std::string &password, const std::string &database,
+            size_t poolSize = 10);
 
-    // 获取连接
-    std::shared_ptr<DbConnection> getConnection();
+  // 获取连接
+  std::shared_ptr<DbConnection> getConnection();
 
 private:
-    // 构造函数
-    DbConnectionPool();
-    // 析构函数
-    ~DbConnectionPool();
+  // 构造函数
+  DbConnectionPool();
+  // 析构函数
+  ~DbConnectionPool();
 
-    // 禁止拷贝
-    DbConnectionPool(const DbConnectionPool&) = delete;
-    DbConnectionPool& operator=(const DbConnectionPool&) = delete;
+  // 禁止拷贝
+  DbConnectionPool(const DbConnectionPool &) = delete;
+  DbConnectionPool &operator=(const DbConnectionPool &) = delete;
 
-    std::shared_ptr<DbConnection> createConnection();
+  std::shared_ptr<DbConnection> createConnection();
 
-    void checkConnections(); // 添加连接检查方法
+  void checkConnections(); // 添加连接检查方法
 
 private:
-    std::string                               host_;
-    std::string                               user_;
-    std::string                               password_;
-    std::string                               database_;
-    std::queue<std::shared_ptr<DbConnection>> connections_;
-    std::mutex                                mutex_;
-    std::condition_variable                   cv_;
-    bool                                      initialized_ = false;
-    std::thread                               checkThread_; // 添加检查线程
+  std::string host_;
+  std::string user_;
+  std::string password_;
+  std::string database_;
+  std::queue<std::shared_ptr<DbConnection>> connections_;
+  std::mutex mutex_;
+  std::condition_variable cv_;
+  bool initialized_ = false;
+  std::thread checkThread_; // 添加检查线程
 };
 
 } // namespace db
-} // namespace http
+} // namespace ww_http
