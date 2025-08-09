fca96deb40827eb1079eb5be9b9336e4782b0442
WW <erhewang2024@163.com>
2025-08-09 16:17:20 +0800

    ...

--- include/utils/db/DbException.h
+++ include/utils/db/DbException.h
@@ -2,18 +2,16 @@
 #include <stdexcept>
 #include <string>
 
-namespace http {
+namespace ww_http {
 namespace db {
 
-class DbException : public std::runtime_error 
-{
+class DbException : public std::runtime_error {
 public:
-    explicit DbException(const std::string& message) 
-        : std::runtime_error(message) {}
-    
-    explicit DbException(const char* message) 
-        : std::runtime_error(message) {}
+  explicit DbException(const std::string &message)
+      : std::runtime_error(message) {}
+
+  explicit DbException(const char *message) : std::runtime_error(message) {}
 };
 
 } // namespace db
-} // namespace http
+} // namespace ww_http
